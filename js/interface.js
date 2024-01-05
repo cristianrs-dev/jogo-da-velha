ebda2338cfb11b65b12fef6c7099393cbdcdf53c
eclipseCJP <cris.tecwodd@gmail.com>
2024-01-05 19:21:13 -0400

    resolver bug da interface

--- js/interface.js
+++ js/interface.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded',quadrante)
 
 function quadrante(){
@@ -10,4 +9,29 @@ function quadrante(){
 
 function cliclado(event){
     console.log(event.target)
+    let quadrante = event.target
+    let posicao = quadrante.id
+    jogada(posicao)
+    atualizaQuadrante()
 }
+
+function atualizaQuadrante(){
+    let quadrante = document.querySelectorAll(".quadrante")
+
+    quadrante.forEach((indice) => {
+        alert(indice.id)
+        let posicao = indice.id
+        simbolo = tabuleiro[posicao]
+
+        if(simbolo != ''){
+
+            
+            indice.innerHTML=`<div class='${simbolo}'></div>`
+
+        }
+
+    });
+
+}
+
+
