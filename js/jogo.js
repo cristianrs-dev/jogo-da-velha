ec1ce1a7725478c848effcf3faa19924baaa2361
eclipseCJP <cris.tecwodd@gmail.com>
2024-01-06 21:58:00 -0400

    verficando campo vazio antes da proxima jogada

--- js/jogo.js
+++ js/jogo.js
@@ -4,10 +4,13 @@ let simbolo = ['x','o']
 
 // essa função rebece a posição em inface e marca a posição com o simbolo
 function jogada(position){
-    tabuleiro[position]=simbolo[jogador]
-    if(jogador ==  0){
-        jogador = 1
-    }else{
-        jogador = 0
+    if(tabuleiro[position]==''){
+        tabuleiro[position]=simbolo[jogador]
+        if(jogador ==  0){
+            jogador = 1
+        }else{
+            jogador = 0
+        }
     }
+    
 }
